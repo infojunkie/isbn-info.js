2c47ae69cd483986e686b0f1a83025ef2365b3b9
infojunkie <karim.ratib@gmail.com>
2022-10-09 12:21:42 -0700

    #17 Add test that reproduces other titles bug

--- test/isbn-detect.js
+++ test/isbn-detect.js
@@ -29,6 +29,10 @@ describe('isbn-detect', function() {
         text: './test/data/test6.txt',
         isbns: ['9780190692681', '9780190692698', '9780190692704', '9780190692674']
       },
+      {
+        text: './test/data/test7.txt',
+        isbns: ['9781138921375', '9781138921368', '9781315686424']
+      },
     ]) {
       assert.deepStrictEqual(isbnDetect(fs.readFileSync(test.text, 'utf-8'), {
         flags: {
